56adcae5cb693f5bc34577ed5d0f1e7746b51ea1
aayush <ar0671362@gmail.com>
2025-09-05 13:02:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga quas iste blanditiis sunt consequuntur? Debitis dicta, impedit aspernatur eveniet aut reprehenderit necessitatibus, consequatur praesentium ipsa illum nemo omnis, beatae aliquid!
+      </p>
+    </div>
+  )
+}
+
+export default App;
